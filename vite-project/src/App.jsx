412a29b413cd1e648dd653dd7c1560fb4b010258
paras231 <sharmaparas553@gmail.com>
2022-07-15 15:05:27 +0530

    initial release of vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Components/Home';
+
+const App = () => {
+  return (
+  <>
+    <Home />
+  <h1>Hello world this is vite here i am the best</h1>
+  </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+  <h1>HOME PAGE HERE</h1>
+  )
+}
+
+export default Home
